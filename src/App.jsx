9c0c90afb1fa67e0556e5b30e925cc20c18511ab
SincerityJayce <vincetacofield@gmail.com>
2025-01-09 20:06:29 +1100

    units can take damage and dissappear

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,28 @@ export default App
 
 
 //defines the different entities
+const centerOnPosition = {transform:"translate(-50%,-50%)"} // reused style
 const entities = {
- Warrior (en){
+ Example (en){
 
+  // example of movement in a line
   const [position] = useSpring(() => ({
      from: en.position,
      to: game.location("enemy goal").position,
-     config:{mass:800}
+     //https://www.react-spring.dev/docs/advanced/config
+     config:{mass:800, clamp:true}
    }),   []  )
 
-  return      <animated.div className="w-8 h-8 flex absolute bg-purple-800" style={{...position}}>{ en.id }</animated.div>
+  return      <animated.div className="w-8 h-8 flex absolute bg-purple-800" style={{...position, ...centerOnPosition}}>{ en.id }</animated.div>
  },
 
  Archer (en){
   const position = en.position
-  return      <div className="w-8 h-8 flex absolute bg-blue-800" style={{...position}}>{ en.id }</div>
+  return      <div className="w-8 h-8 flex absolute bg-blue-800" style={{...position, ...centerOnPosition}}>{ en.id }</div>
  },
  Turret (en){
   const position = en.position
-  return      <div className="w-8 h-8 flex absolute bg-red-800" style={{...position}}>{ en.id }</div>
+  return      <div className="w-8 h-8 flex absolute bg-red-800" style={{...position, ...centerOnPosition}}>{ en.id }</div>
  },
 }
 
@@ -52,8 +55,12 @@ function ScreenContainer(){
   <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' id="position field in center of container">
    <div style={{transform:`scale(${scale})`, width, aspectRatio}} className={"relative bg-slate-800"} id="field">
 
-    {/* show all entities */}
-    {game.Entities.map(en=>entities[en.type](en))}
+    {/* show all entities */
+     game.Entities.map(function Entity (en){
+      const Form = entities[en.type]
+      return <Form key={en.id+" -  entity"} {...en}/>}
+     )
+    }
     
    </div>
   </div>
@@ -63,9 +70,21 @@ function ScreenContainer(){
 
 
 // holds the game state
+const select = {
+ entity:id=>s=>s.entities.find(e=>e.id==id),
+ location:id=>s=>s.locations.find(e=>e.id==id),
+}
+const method = {
+ damageEntity:(id, dmg)=>s=>{
+  let unit = select.entity(id)(s)
+  unit.health -=dmg
+  if(unit.health<=0)method.removeEntity(id)(s)
+ },
+ removeEntity:id=>s=>{s.entities = s.entities.filter(e=>e.id!=id)}
+}
 const game = sweet({ 
  entities:[
-  {id:4, type:"Warrior", position:{left:50, bottom:600}},
+  {id:4, type:"Example", health:50, position:{left:50, bottom:50}},
   {id:3, type:"Archer", position:{left:400, bottom:500}},
   {id:2, type:"Turret", position:{left:500, bottom:500}},
  ],
@@ -73,8 +92,12 @@ const game = sweet({
   {id:"enemy goal", position:{left:300, bottom:800}},
   {id:"my goal", position:{left:300, bottom:100}}
  ],
+ select,
+ method,
 })
-.selectors({
- entity:id=>s=>s.entities.find(e=>e.id==id),
- location:id=>s=>s.locations.find(e=>e.id==id),
-})
+
+
+
+// For units
+// A health stat
+// an event that removes them when their health stat reaches 
